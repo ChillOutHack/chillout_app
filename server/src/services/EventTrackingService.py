c2adee861d7c153d30a437b545db6eebf82c8a88
aliang <a.liang1989@gmail.com>
2014-04-26 13:05:21 -0400

    stashing Service contracts

--- server/src/services/EventTrackingService.py
+++ server/src/services/EventTrackingService.py
@@ -1,4 +1,5 @@
 from types import DictType
+from datetime import datetime
 
 class EventTrackingService (object):
     """
@@ -7,10 +8,24 @@ class EventTrackingService (object):
     """
 
     @staticmethod
-    def track_event(eventObject):
+    def track_event (eventObject):
         """
             Tracks an event, for now accepts a Dictionary... might turn into JSON string, or individual params?
 
             @type eventObject: DictType
         """
         assert isinstance(eventObject, DictType)
+
+    @staticmethod
+    def fetch_events (start_time, end_time):
+        """
+            Fetches events that fall between the L{start_time} and L{end_time}.
+
+            @type start_time: datetime
+            @type end_time: datetime
+            @precondition: start_time < end_time
+        """
+        assert isinstance(start_time, datetime), type(start_time)
+        assert isinstance(end_time, datetime), type(end_time)
+        assert start_time < end_time
+
