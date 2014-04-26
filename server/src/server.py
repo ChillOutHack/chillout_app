9bf6b0b8bd6b45f2b6b1ef5d138b4339bfb8c74e
Jenny Schweers <orange.jenny@gmail.com>
2014-04-26 11:33:58 -0400

    Routes for activating and deactivating device

--- server/src/server.py
+++ server/src/server.py
@@ -1,3 +1,4 @@
+from handlers import handler
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,5 +6,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/activate", methods=['GET', 'POST'])
+def activate():
+    return handler.activate()
+
+@app.route("/deactivate", methods=['GET','POST'])
+def inactivate():
+    return handler.deactivate()
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
