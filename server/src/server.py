f097ad0e723be5a13c6ac1ccbdc912599c9dd3d3
aliang <a.liang1989@gmail.com>
2014-04-26 11:25:35 -0400

    base template (no content)

--- server/src/server.py
+++ server/src/server.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+#for now define routes inline with server.py
 @app.route("/")
-def hello():
-    return "Hello World!"
+def render_main():
+    """
+        Renders the main (base) page.
+    """
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run()
