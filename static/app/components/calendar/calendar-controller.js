293ddeb8f8c03295efdb02829fc715173c8e502c
Vik <vik.paruchuri@gmail.com>
2014-04-27 11:10:47 -0400

    Add in timeline

--- static/app/components/calendar/calendar-controller.js
+++ static/app/components/calendar/calendar-controller.js
@@ -1,3 +1,47 @@
 chillout.controller('CalendarCtrl', function ($scope) {
-	console.log('CalendarCtrl running');
+
+    $scope.allData = [];
+    $scope.ids = [];
+
+    var options = {
+      "width":  "100%",
+      "height": "600px",
+      "style": "box",
+      "editable": true,
+        "cluster": true,
+        "showNavigation": true
+    };
+
+    $scope.options = options;
+
+	var fetchData = function (callback) {
+        console.log("fetching");
+        $.ajax('/api/data', {
+            success: callback
+        });
+    };
+
+    var renderLine = function(data){
+        data = JSON.parse(data);
+        data = data.events;
+        for(var i = 0; i < data.length; i++){
+            if($scope.ids.indexOf(data[i].id) === -1){
+                if(data[i].peltier > 30){
+                    $scope.allData.push({
+                        start: Date.parse(data[i].created),
+                        content: data[i].peltier
+                    });
+                    $scope.ids.push(data[i].id);
+                }
+            }
+        }
+    };
+
+    window.setInterval(function(){
+        //update function by polling every 3.5 seconds
+        fetchData(function(d){
+            renderLine(d);
+        });
+    }, 3500);
+
 });
