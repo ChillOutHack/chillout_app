546c3d2ad4e1879df1623483695cb2d2b84e0175
Adam Laughlin <adam.laughlin@gmail.com>
2014-04-26 20:24:34 -0400

    make flask's jinja play well with angular

--- server.py
+++ server.py
@@ -6,7 +6,18 @@ from datetime import datetime
 from path import path
 import os
 
-app = Flask(__name__)
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
+
+app = CustomFlask(__name__)
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(main)
 
