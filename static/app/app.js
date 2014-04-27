77b0211273886b56da9ae3ab07a694e99b170ecf
Adam Laughlin <adam.laughlin@gmail.com>
2014-04-27 09:25:21 -0400

    map working, and added time series to map page for presentation

--- static/app/app.js
+++ static/app/app.js
@@ -24,4 +24,12 @@ var chillout = angular.module('chillout', [
 // run is where we set initial rootscope properties
 .run(function ($rootScope) {
 	console.log('Running chillout module');
+})
+
+.controller('RouteCtrl',function ($scope,$location) {
+	$scope.reRoute = function (path) {
+		console.log('switching path to:',path);
+		$location.path(path);
+		$scope.activeBtn = path.slice(1);
+	}
 });
--- static/app/components/map/map-controller.js
+++ static/app/components/map/map-controller.js
@@ -1,3 +1,14 @@
 chillout.controller('MapCtrl', function ($scope) {
 	console.log('MapCtrl running');
+	$scope.$on('$viewContentLoaded',function () {
+		var map = L.map('mapcontainer').setView([42.359802,-71.126647], 13);
+		// to switch these to other tiles, check out the examples at https://github.com/shramov/leaflet-plugins
+		// the google api allows for customizable google maps icons
+		L.tileLayer('http://d.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
+			attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+			maxZoom: 18
+		}).addTo(map);
+		var marker = L.marker([42.359802,-71.126647]).addTo(map);
+		// console.log('mapcontainer',mapcontainer);
+	});
 });
